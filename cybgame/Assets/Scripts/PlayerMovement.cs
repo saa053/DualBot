ab780965a0baef47b5d770cff630d766c218ba2a
Stian Aasheim <stian.aasheim98@gmail.com>
2024-01-16 12:05:43 +0100

    update unity version

---
+++ cybgame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public void OnInput(InputAction.CallbackContext context)
+    {
+        //Vector2 newInput = context.ReadValue<Vector2>();
+    }
+}

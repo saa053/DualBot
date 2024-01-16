5835031c98bbdb01e399091c2a81c67901755ac7
Stian Aasheim <stian.aasheim98@gmail.com>
2024-01-16 12:40:21 +0100

    add player movement (controller and keyboard)

--- cybgame/Assets/Scripts/PlayerMovement.cs
+++ cybgame/Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float speed;
+    Vector2 input;
+    Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
     public void OnInput(InputAction.CallbackContext context)
     {
-        //Vector2 newInput = context.ReadValue<Vector2>();
+        input = context.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        body.velocity = input * speed;
     }
 }
